56ed6aeb81a1d804be2b0162145d95a40516fc60
Michael Wolf <Michael Wolf>
2017-05-06 00:17:54 -0400

    Adjusted model for users

--- models/user.js
+++ models/user.js
@@ -1,10 +1,16 @@
 module.exports = function(sequelize, Sequelize) {
     var User = sequelize.define('user', {
         id: { autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER },
+
         firstname: { type: Sequelize.STRING, notEmpty: true },
         lastname: { type: Sequelize.STRING, notEmpty: true },
         username: { type: Sequelize.TEXT },
-        about: { type: Sequelize.TEXT },
+
+        age: { type: Sequelize.TEXT },
+        location: { type: Sequelize.STRING },
+        is_male: { type: Sequelize.BOOLEAN },
+        seeking_male: { type: Sequelize.BOOLEAN },
+
         email: { type: Sequelize.STRING, validate: { isEmail: true } },
         password: { type: Sequelize.STRING, allowNull: false },
         last_login: { type: Sequelize.DATE },
